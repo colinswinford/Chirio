86f5b3f60878f7a8868a0e3d5d83856ba2af98e5
colinswinford <colinswinford@gmail.com>
2024-11-03 19:13:26 -0500

    Initial Commit

---
+++ Chirio/ChirioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChirioApp.swift
+//  Chirio
+//
+//  Created by Colin Swinford on 11/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ChirioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChirioTests/ChirioTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChirioTests.swift
+//  ChirioTests
+//
+//  Created by Colin Swinford on 11/3/24.
+//
+
+import Testing
+@testable import Chirio
+
+struct ChirioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
